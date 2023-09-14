198bdab7d7e43c752b232109c010d00fe346c9df
Arunsardar <arunsardar025@gmail.com>
2023-09-14 23:54:48 +0530

    2nd commited by Arun Sardar

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -2,8 +2,17 @@ import { useState } from "react";
 import axios from "axios";
 import "./FileUpload.css";
 const FileUpload = ({ contract, account, provider }) => {
+    const [file, setFile]=useState(null);
+    const [fileName, setFileName]=useState("No Image Selected");
     const handleSubmit = async(e) => {
+        e.preventDefault();
+        if(file){
+            try{
 
+            }catch(e){
+                alert("Unable to upload to Pinata");
+            }
+        }
     }
 
     const retrieveFile=()=>{
@@ -14,9 +23,17 @@ const FileUpload = ({ contract, account, provider }) => {
             <label htmlFor="file-upload" className="choose">
                 Choose Image
             </label>
-            <input disabled={!account} type="file" id="file-upload" name="data" onChange={retrieveFile}/>
-            <span>Image:#temprory.png</span>
-            <button type="submit" className="upload">Upload File</button>
+            <input 
+            disabled={!account} 
+            type="file" 
+            id="file-upload" 
+            name="data" 
+            onChange={retrieveFile}              
+            />
+            <span className="textArea">Image:{fileName} </span>
+            <button type="submit" className="upload">
+            Upload File
+            </button>
         </form>
     </div>;
 };
